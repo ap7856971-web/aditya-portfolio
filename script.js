5fab5562ab857d1768ebe04b3f21b441240e9d7c
Aditya Pal <ap7856971@gmail.com>
2026-09-15 17:33:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const menuBtn = document.getElementById('menuBtn'), nav = document.getElementById('navLinks'); menuBtn.addEventListener('click', () => nav.classList.toggle('open')); document.querySelectorAll('.nav-links a').forEach(a => a.addEventListener('click', () => nav.classList.remove('open')));
